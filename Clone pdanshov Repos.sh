452dd877d29987720e8d31d13e56cae2d8d1cb74
pdanshov <pdanshv@gmail.com>
2014-10-27 12:35:56 -0400

    Mon Oct 27 12:35:57 EDT 2014

---
+++ Clone pdanshov Repos.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#############################
+#####
+###
+##			Peter Danshov 10.27.14
+##			pdanshv@gmail.com
+##			Auto-clone all github repos
+##
+###
+#####
+#############################
+
+cd C:\
+mkdir GitHub_Folders
+cd GitHub_Folders
+git clone https://github.com/pdanshov/Batch-Scripts.git ; sleep 3
+git clone https://github.com/pdanshov/Registry-Entries.git ; sleep 3
+git clone https://github.com/pdanshov/Shell-Scripts.git ; sleep 3
+git clone https://github.com/pdanshov/VB-Scripts.git ; sleep 3
+#git clone https://github.com/pdanshov/R-Scripts.git ; sleep 3
+git clone https://github.com/pdanshov/Python-Scripts.git ; sleep 3
+#git clone https://github.com/pdanshov/Processing-Code.git ; sleep 3
+#git clone https://github.com/pdanshov/MAX-Code.git ; sleep 3
+git clone https://github.com/pdanshov/C-Reusable-Code.git ; sleep 3
+git clone https://github.com/pdanshov/MS-Office-Scripts.git ; sleep 3
+#git clone https://github.com/pdanshov/Java-Code.git ; sleep 3
+#git clone https://github.com/pdanshov/Arduino-Code.git ; sleep 3
+#git clone https://github.com/pdanshov/AutoCAD.git ; sleep 3
+#git clone 
+#git clone 
+#git clone 
+#git clone 
+#git clone 
+#git clone 
+
+
