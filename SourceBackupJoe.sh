00624208553069d7a05d1fc1c4a3f876a28226ad
pdanshov <pdanshv@gmail.com>
2014-12-09 05:36:17 -0500

    Tue Dec  9 05:35:33 EST 2014

---
+++ SourceBackupJoe.sh
@@ -0,0 +1,74 @@
+
+#!/bin/sh
+(
+#########################################################################
+####
+###
+## 						 Peter Danshov 11.18.14
+##                         pdanshv@gmail.com
+## 			A bash script to incrementally backup source code
+##
+##       Logs are in C:\MinGW\msys\1.0\
+###
+####
+#########################################################################
+
+set -x
+
+umount /SourceBakDir
+umount /C
+mount X: /SourceBakDir
+#mount /C/ /cdrive
+mount "C:/" "/C"
+
+cd /SourceBakDir
+#ls -al
+
+#cd /C
+#ls -al
+
+
+# Is there a TRY CATCH command in Bash?
+# No.
+# Bash doesn't have all luxury as you can find in many programming language and also it's true that bash doesn't have less luxury than other programming language.
+# There is not really a try/catch in bash but you can achieve a quite similar behavior using && or ||
+# Using ||
+# if command1 fails then you will run command2 as follows
+# command1 || command2
+# similar using && you can run command2 if command1 successful
+# Now if you want exact flavor of try/catch then try it
+# { # this is my bash try block
+    # command1 &&
+    # #save your output
+# } || { # this is catch block
+    # # save log for exception 
+# }
+
+### rsync -artvu --delete "/C/Users/Administrator/Desktop" "Backup/SourceBackup/Joe/"
+#sleep 3
+### rsync -artvu --delete "/C/Program Files/Open Systems, Inc/TRAVERSE MODS" "Backup/SourceBackup/Joe/"
+
+
+# Bash Experimental Try/Except Block
+########################################################################################################
+{
+	rsync -artvu --delete "/C/Users/Administrator/Desktop" "Backup/SourceBackup/Joe/" && 
+	rsync -artvu --delete "/C/Program Files/Open Systems, Inc/TRAVERSE MODS" "Backup/SourceBackup/Joe/"
+} || {
+	rsync -artvu --delete "/C/Program Files/Open Systems, Inc/TRAVERSE MODS" "Backup/SourceBackup/Joe/"
+}
+	
+
+echo -e "\n
+" >> Backup/SourceBackup/Joe/log.log
+echo `date` >> Backup/SourceBackup/Joe/log.log
+# echo "$(date) - completed." >> Backup/SourceBackup/Joe/log.log
+
+umount /SourceBakDir
+umount /C
+
+) 2>&1 | tee /SourceBak_Bash.log
+
+
+
+
